0e4258ad7495b01542a7c1a6ff8dd93d08f5bb4e
lvnaliyev <53705814+lvnaliyev@users.noreply.github.com>
2021-04-10 15:01:05 +0900

    Update accordionT.js

--- accordionT.js
+++ accordionT.js
@@ -6,13 +6,13 @@ const navSlide = () => {
     burger.addEventListener (`click`,() => {
     		/*TOGGLE THE NAVIGATION*/
     		 nav.classList.toggle (`nav-active`);
-    		/* Anumate Links */
+    		/* Anumate Links
    		   navLinks.forEach((link,index) => {
     	  	if (link.style.animation){
         			link.style.animation = ``
       		}else {
         			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
-      		}
+      		}*/
    		   });
     	  //Burger Animation
     	 burger.classList.toggle(`toggle`);
